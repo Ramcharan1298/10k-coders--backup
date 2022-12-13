77e9f70bf050bb74ecb4ff010a4f31d6e69662d0
Ramcharan1298 <yachamaneniramcharan@gmail.com>
2022-12-13 14:35:00 +0530

    new one

---
+++ ReactJS/forms-crud/src/App.js
@@ -0,0 +1,13 @@
+
+import './style.css';
+import Form1 from './FORMS--C_R_U_D/Form1';
+
+function App() {
+  return (
+    <div className="App">
+      <Form1/>
+    </div>
+  );
+}
+
+export default App;
